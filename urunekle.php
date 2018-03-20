8575e5b3a37b653057841aadd4c2905142c52a93
suleymansaaglam <37334102+anderial55@users.noreply.github.com>
2018-03-20 14:50:42 +0300

    Süleyman sağlam
    ürünleri ekleyip markasını ayarlayıp fiyatını ayarladım .

--- urunekle.php
+++ urunekle.php
@@ -32,13 +32,19 @@
 		fopen("urun.txt","w");
 	}
 	if ($_POST["ekle"])
+	{ if ($_POST["kaydet"])
 	{
-		
-		
-		
-		
-		
-		
-		
+		if($_POST["uadi"]=="" || $_POST["umarkasi"]=="" || $_POST["ufiyati"]=="")
+		{
+			echo"Eksik bilgileri doldurunuz";
+		}
+		else
+		{
+			$dosya=fopen("bilgi.txt","a");
+			$yazilacak=$_POST["uadi"]."-".$_POST["umarkasi"]."-".$_POST["ufiyati"].
+			fputs($dosya,$yazilacak);
+			echo"********Ürün eklenmiştir*********";
+		}
 	}
 ?>
+			
